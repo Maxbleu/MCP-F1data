8c1985a38905aadf7974184a12053cd7b9ec412d
maxbleu <juanfranciscomm03@gmail.com>
2025-10-13 18:25:12 +0200

    FIX: delete one slash from f1db base path

--- mcp_f1data/utils/request_utils.py
+++ mcp_f1data/utils/request_utils.py
@@ -2,7 +2,7 @@ import os, requests
 
 def launch_request_f1db(path: str):
     headers = {"Authorization": f"Bearer {os.getenv("JWT_TOKEN")}"}
-    response = requests.get(url=f"https://apif1db-production.up.railway.app/api/{path}",headers=headers)
+    response = requests.get(url=f"https://apif1db-production.up.railway.app/api{path}",headers=headers)
     if response.status_code == 200:
         json = response.json()
         return json
