fdffbae9b10b53466af44a47cb6f8ba25f2d8ec9
maxbleu <juanfranciscomm03@gmail.com>
2025-10-22 12:49:45 +0200

    ADD: grand prix tools

--- mcp_f1data/tools/f1data_tools.py
+++ mcp_f1data/tools/f1data_tools.py
@@ -3,6 +3,7 @@ from .driver_tools import DriverTools
 from .telemetry_tools import TelemetryTools
 from .constructor_tools import ConstructorTools
 from .circuit_tools import CircuitTools
+from .grand_prix_tools import GrandPrixTools
 
 def register_f1data_tools(mcp:FastMCP):
     """Register all F1 tools with the MCP server"""
@@ -11,3 +12,4 @@ def register_f1data_tools(mcp:FastMCP):
     DriverTools.__register_mcp_tools__(mcp=mcp)
     ConstructorTools.__register_mcp_tools__(mcp=mcp)
     CircuitTools.__register_mcp_tools__(mcp=mcp)
+    GrandPrixTools.__register_mcp_tools__(mcp=mcp)
---
+++ mcp_f1data/tools/grand_prix_tools.py
@@ -0,0 +1,237 @@
+import json
+from pydantic import Field
+from fastmcp import FastMCP
+from .base_tools import BaseTools
+from ..utils import launch_request_f1db
+
+class GrandPrixTools(BaseTools):
+    def __init__(self, mcp: FastMCP):
+        super().__init__(mcp=mcp)
+
+    BASE_PATH = "/grand_prix"
+
+    @classmethod
+    def __register_mcp_tools__(cls, mcp: FastMCP) -> None:
+        """Register all grand prix tools with MCP"""
+
+        @mcp.tool(name="get_grand_prix_by_id")
+        async def get_grand_prix_by_id(
+            grand_prix_id: str = Field(title="grand_prix_id", description="Grand Prix's identifier")
+        ) -> json:
+            """
+            Get grand prix historical information:
+                abbreviation, str
+                name, str
+                total_races_held, int
+                short_name, str
+                id, str
+                full_name, str
+                country_id, str
+            """
+            result = launch_request_f1db(f"{cls.BASE_PATH}/{grand_prix_id}")
+            return result
+
+        @mcp.tool(name="get_grand_prix_by_id_and_year")
+        async def get_grand_prix_by_id_and_year(
+            grand_prix_id: str = Field(title="grand_prix_id", description="Grand Prix's identifier"),
+            year: int = Field(title="year", description="Year of the season")
+        ) -> json:
+            """
+            Get grand prix of especific season:
+                date, str
+                drivers_championship_decider, bool
+                qualifying_1_time, str
+                sprint_race_time, str
+                time, str
+                direction, str
+                constructors_championship_decider, bool
+                free_practice_2_date, date
+                qualifying_2_date, date
+                warming_up_date, date
+                official_name, str
+                course_length, float
+                pre_qualifying_date, date
+                free_practice_2_time, str
+                qualifying_2_time, str
+                warming_up_time, str
+                year, int
+                turns, int
+                free_practice_3_date, date
+                qualifying_date, date
+                qualifying_format, str
+                laps, int
+                pre_qualifying_time, str
+                free_practice_3_time, str
+                qualifying_time, str
+                id, int
+                circuit_type, str
+                distance, float
+                free_practice_4_date, date
+                sprint_qualifying_date, date
+                round, int
+                scheduled_laps, int
+                free_practice_1_date, date
+                free_practice_4_time, str
+                sprint_qualifying_time, str
+                scheduled_distance, float
+                free_practice_1_time, str
+                qualifying_1_date, date
+                sprint_race_date, date
+                grand_prix, obj
+                circuit, ob
+            """
+            result = launch_request_f1db(f"{cls.BASE_PATH}/{grand_prix_id}/{year}")
+            return result
+
+        @mcp.tool(name="get_grand_prix_chronology_by_id")
+        async def get_grand_prix_chronology_by_id(
+            grand_prix_id: str = Field(title="grand_prix_id", description="Grand Prix's identifier")
+        ) -> json:
+            """
+            Get a list of F1 Grands Prix held at different circuits:
+                date, str
+                drivers_championship_decider, bool
+                qualifying_1_time, str
+                sprint_race_time, str
+                time, str
+                direction, str
+                constructors_championship_decider, bool
+                free_practice_2_date, date
+                qualifying_2_date, date
+                warming_up_date, date
+                official_name, str
+                course_length, float
+                pre_qualifying_date, date
+                free_practice_2_time, str
+                qualifying_2_time, str
+                warming_up_time, str
+                year, int
+                turns, int
+                free_practice_3_date, date
+                qualifying_date, date
+                qualifying_format, str
+                laps, int
+                pre_qualifying_time, str
+                free_practice_3_time, str
+                qualifying_time, str
+                id, int
+                circuit_type, str
+                distance, float
+                free_practice_4_date, date
+                sprint_qualifying_date, date
+                round, int
+                scheduled_laps, int
+                free_practice_1_date, date
+                free_practice_4_time, str
+                sprint_qualifying_time, str
+                scheduled_distance, float
+                free_practice_1_time, str
+                qualifying_1_date, date
+                sprint_race_date, date
+                grand_prix, obj
+                circuit, ob
+            """
+            result = launch_request_f1db(f"{cls.BASE_PATH}/{grand_prix_id}/chronology")
+            return result
+
+        @mcp.tool(name="get_grand_prix_drivers_championship_decider_by_id")
+        async def get_grand_prix_drivers_championship_decider_by_id(
+            grand_prix_id: str = Field(title="grand_prix_id", description="Grand Prix's identifier")
+        ) -> json:
+            """
+            Get a list of F1 Grands Prix held at different circuits that decided the Constructors' Championship:
+                date, str
+                drivers_championship_decider, bool
+                qualifying_1_time, str
+                sprint_race_time, str
+                time, str
+                direction, str
+                constructors_championship_decider, bool
+                free_practice_2_date, date
+                qualifying_2_date, date
+                warming_up_date, date
+                official_name, str
+                course_length, float
+                pre_qualifying_date, date
+                free_practice_2_time, str
+                qualifying_2_time, str
+                warming_up_time, str
+                year, int
+                turns, int
+                free_practice_3_date, date
+                qualifying_date, date
+                qualifying_format, str
+                laps, int
+                pre_qualifying_time, str
+                free_practice_3_time, str
+                qualifying_time, str
+                id, int
+                circuit_type, str
+                distance, float
+                free_practice_4_date, date
+                sprint_qualifying_date, date
+                round, int
+                scheduled_laps, int
+                free_practice_1_date, date
+                free_practice_4_time, str
+                sprint_qualifying_time, str
+                scheduled_distance, float
+                free_practice_1_time, str
+                qualifying_1_date, date
+                sprint_race_date, date
+                grand_prix, obj
+                circuit, ob
+            """
+            result = launch_request_f1db(f"{cls.BASE_PATH}/{grand_prix_id}/championship_decider/drivers")
+            return result
+
+        @mcp.tool(name="get_grand_prix_constructors_championship_decider_by_id")
+        async def get_grand_prix_constructors_championship_decider_by_id(
+            grand_prix_id: str = Field(title="grand_prix_id", description="Grand Prix's identifier")
+        ) -> json:
+            """
+            Get a list of F1 Grands Prix held at different circuits that decided the Constructors' Championship:
+                date, str
+                drivers_championship_decider, bool
+                qualifying_1_time, str
+                sprint_race_time, str
+                time, str
+                direction, str
+                constructors_championship_decider, bool
+                free_practice_2_date, date
+                qualifying_2_date, date
+                warming_up_date, date
+                official_name, str
+                course_length, float
+                pre_qualifying_date, date
+                free_practice_2_time, str
+                qualifying_2_time, str
+                warming_up_time, str
+                year, int
+                turns, int
+                free_practice_3_date, date
+                qualifying_date, date
+                qualifying_format, str
+                laps, int
+                pre_qualifying_time, str
+                free_practice_3_time, str
+                qualifying_time, str
+                id, int
+                circuit_type, str
+                distance, float
+                free_practice_4_date, date
+                sprint_qualifying_date, date
+                round, int
+                scheduled_laps, int
+                free_practice_1_date, date
+                free_practice_4_time, str
+                sprint_qualifying_time, str
+                scheduled_distance, float
+                free_practice_1_time, str
+                qualifying_1_date, date
+                sprint_race_date, date
+                grand_prix, obj
+                circuit, ob
+            """
+            result = launch_request_f1db(f"{cls.BASE_PATH}/{grand_prix_id}/championship_decider/constructors")
+            return result
