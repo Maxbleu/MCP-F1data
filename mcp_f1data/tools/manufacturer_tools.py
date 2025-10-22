365339a8a3f9b2eaf422902e7069a7afe2d520dd
maxbleu <juanfranciscomm03@gmail.com>
2025-10-22 14:01:55 +0200

    MDF: remove get_all tyre and engine manufacturer

--- mcp_f1data/tools/manufacturer_tools.py
+++ mcp_f1data/tools/manufacturer_tools.py
@@ -14,27 +14,6 @@ class ManufacturerTools(BaseTools):
     def __register_mcp_tools__(cls, mcp: FastMCP) -> None:
         """Register all manufacturer tools with MCP"""
 
-        @mcp.tool(name="get_tyre_manufacturers")
-        async def get_tyre_manufacturers() -> json:
-            """
-            Get every tyre manufacturer:
-                id, str
-                name, str
-                country_id, str
-                best_starting_grid_position, int
-                best_race_result, int
-                total_race_entries, int
-                total_race_starts, int
-                total_race_wins, int
-                total_race_laps, int
-                total_podiums, int
-                total_podium_races, int
-                total_pole_positions, int
-                total_fastest_laps, int
-            """
-            result = launch_request_f1db(f"{cls.BASE_PATH}/tyres")
-            return result
-
         @mcp.tool(name="get_tyres_manufacturer_by_id")
         async def get_tyres_manufacturer_by_id(
             tyre_manufacturer_id: str = Field(title="tyre_manufacturer_id", description="Tyre manufacturer's identifier")
@@ -56,30 +35,6 @@ class ManufacturerTools(BaseTools):
                 total_fastest_laps, int
             """
             result = launch_request_f1db(f"{cls.BASE_PATH}/tyres/{tyre_manufacturer_id}")
-            return result
-
-        @mcp.tool(name="get_engine_manufacturers")
-        async def get_engine_manufacturers() -> json:
-            """
-            Get every engine manufacturer:
-                id, str
-                name, str
-                country_id, str
-                best_championship_position, int
-                best_starting_grid_position, int
-                best_race_result, int
-                total_race_entries, int
-                total_race_starts, int
-                total_race_wins, int
-                total_race_laps, int
-                total_podiums, int
-                total_podium_races, int
-                total_points, float
-                total_championship_points, float
-                total_pole_positions, int
-                total_fastest_laps, int
-            """
-            result = launch_request_f1db(f"{cls.BASE_PATH}/engines")
             return result
 
         @mcp.tool(name="get_engine_manufacturer_by_id")
