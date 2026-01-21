c0e459165f3707644eba40276263920b4732f417
maxbleu <juanfranciscomm03@gmail.com>
2026-01-21 18:56:14 +0100

    MDF: documentation race's endpoints

--- mcp_f1data/tools/race_tools.py
+++ mcp_f1data/tools/race_tools.py
@@ -13,82 +13,82 @@ class RaceTools(BaseTools):
     @staticmethod
     def get_free_practice_1_by_id(race_id: int) -> json:
         """Get a driver's list result of free practice 1 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_1")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/1")
 
     @staticmethod
     def get_free_practice_1_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of free practice 1 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_1/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/1/{driver_id}")
 
     @staticmethod
     def get_free_practice_2_by_id(race_id: int) -> json:
         """Get a driver's list result of free practice 2 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_2")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/2")
 
     @staticmethod
     def get_free_practice_2_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of free practice 2 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_2/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/2/{driver_id}")
 
     @staticmethod
     def get_free_practice_3_by_id(race_id: int) -> json:
         """Get a driver's list result of free practice 3 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_3")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/3")
 
     @staticmethod
     def get_free_practice_3_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of free practice 3 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_3/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/3/{driver_id}")
 
     @staticmethod
     def get_free_practice_4_by_id(race_id: int) -> json:
         """Get a driver's list result of free practice 4 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_4")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/4")
 
     @staticmethod
     def get_free_practice_4_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of free practice 4 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/practice_4/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/free_practice/4/{driver_id}")
 
     @staticmethod
     def get_qualy_1_by_id(race_id: int) -> json:
         """Get a driver's list result of qualy 1 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_1")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/1")
 
     @staticmethod
     def get_qualy_1_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of qualy 1 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_1/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/1/{driver_id}")
 
     @staticmethod
     def get_qualy_2_by_id(race_id: int) -> json:
         """Get a driver's list result of qualy 2 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_2")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/2")
 
     @staticmethod
     def get_qualy_2_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of qualy 2 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_2/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/2/{driver_id}")
 
     @staticmethod
     def get_qualy_3_by_id(race_id: int) -> json:
         """Get a driver's list result of qualy 3 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_3")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/3")
 
     @staticmethod
     def get_qualy_3_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver result of qualy 3 of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy_3/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/3/{driver_id}")
 
     @staticmethod
     def get_pre_qualy_result_by_id(race_id: int) -> json:
         """Get a driver's list pre result of qualy of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/pre_result")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/pre")
 
     @staticmethod
     def get_pre_qualy_result_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get a driver pre result of qualy of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/pre_result/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/qualy/pre/{driver_id}")
 
     @staticmethod
     def get_starting_grid_by_id(race_id: int) -> json:
@@ -124,6 +124,36 @@ class RaceTools(BaseTools):
     def get_race_result_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
         """Get driver race result of specific race"""
         return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/race/{driver_id}")
+
+    @staticmethod
+    def get_sprint_qualy_1_result_by_id(race_id: int) -> json:
+        """Get a driver's list result of sprint qualy 1 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/1")
+
+    @staticmethod
+    def get_sprint_qualy_1_result_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
+        """Get a driver result of sprint qualy 1 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/1/{driver_id}")
+
+    @staticmethod
+    def get_sprint_qualy_2_result_by_id(race_id: int) -> json:
+        """Get a driver's list result of sprint qualy 2 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/2")
+
+    @staticmethod
+    def get_sprint_qualy_2_result_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
+        """Get a driver result of sprint qualy 2 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/2/{driver_id}")
+
+    @staticmethod
+    def get_sprint_qualy_3_result_by_id(race_id: int) -> json:
+        """Get a driver's list result of sprint qualy 3 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/3")
+
+    @staticmethod
+    def get_sprint_qualy_3_result_by_id_and_driver_id(race_id: int, driver_id: str) -> json:
+        """Get a driver result of sprint qualy 3 of specific race"""
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/sprint_qualy/3/{driver_id}")
 
     @staticmethod
     def get_sprint_qualy_by_id(race_id: int) -> json:
@@ -183,19 +213,19 @@ class RaceTools(BaseTools):
     @staticmethod
     def get_driver_standing_by_id(race_id: int) -> json:
         """Get driver standing list of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/driver_standing")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/standings/drivers")
 
     @staticmethod
     def get_driver_standing_by_id_and_driver(race_id: int, driver_id: str) -> json:
         """Get driver standing of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/driver_standing/{driver_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/standings/drivers/{driver_id}")
 
     @staticmethod
     def get_constructor_standing_by_id(race_id: int) -> json:
         """Get constructor standing list of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/constructor_standing")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/standings/constructors")
 
     @staticmethod
     def get_constructor_standing_by_id_and_constructor(race_id: int, constructor_id: str) -> json:
         """Get constructor standing of specific race"""
-        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/constructor_standing/{constructor_id}")
+        return launch_request_f1db(f"{RaceTools.BASE_PATH}/{race_id}/standings/constructors/{constructor_id}")
